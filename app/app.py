be98a1fb8323d4a7f261de5b7b11fcaf1437d59b
greciasaucedo <149830471+greciasaucedo@users.noreply.github.com>
2025-08-20 14:24:27 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"message": "hola me llamo toño está jalando a toda madre"})
+    return jsonify({"message": "hola, soy grecia. ya ahora si funciona!!! :)"})
 
 if __name__ == '__main__':
     app.run(threaded=True, host='0.0.0.0', port=3000)
